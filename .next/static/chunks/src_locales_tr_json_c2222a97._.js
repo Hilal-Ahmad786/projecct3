9b3df005d95284c75bca0f3e4d05bc5672a8fec1
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2025-10-31 14:33:30 +0300

    logo updateddd

---
+++ .next/static/chunks/src_locales_tr_json_c2222a97._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_locales_tr_json_b5bf6715._.js"
+],
+    source: "dynamic"
+});
